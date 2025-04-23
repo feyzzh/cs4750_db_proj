8a2694113c90e0b071faf842f00977199acd1632
ket6cc <ket6cc@virginia.edu>
2025-04-23 00:06:28 -0400

    added page to view all entries

--- cavcore/hooswellapp/urls.py
+++ cavcore/hooswellapp/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
     path('add_fitness', views.add_fitness_log, name='add_fitness_log'),
     path('stats/', views.stats_dashboard, name='stats_dashboard'),
     path('nutri-dash/', views.nutrition_dashboard, name='nutri_dash'),
-    path('dashboard/', views.dashboard, name='dashboard')
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('entry_manager/', views.entry_manager, name='entry_manager'),
+    # path('update_entry/', views.update_entry, name='update_entry'),
+    # path('delete_entry/', views.delete_entry, name='delete_entry'),
 
 ]
